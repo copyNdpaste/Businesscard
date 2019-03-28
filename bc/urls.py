eb5d10495014061b1d0f91ce6b707a9c23e4630d
dhlocaladm <julsnutini@naver.com>
2019-03-28 15:07:15 +0900

    [ 명함 관리 페이지] Simple Reverse URL in html file Use app_name and path name 2019.3.28

--- bc/urls.py
+++ bc/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from bc import views
 
+app_name = 'bc'
 urlpatterns = [
     path('create/', views.create),
-    path('list/', views.list),
+    path('list/', views.list, name='list'),
     path('list/update/', views.update),
     path('list/delete/', views.delete),
     path('list/search/', views.search),
